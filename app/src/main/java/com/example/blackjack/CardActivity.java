5b08f8093b193fa5339ab4cd30151bc84743176d
jtn3 <jtn3@illinois.edu>
2021-01-20 19:32:19 -0600

    ui and internet access done

--- app/src/main/java/com/example/blackjack/CardActivity.java
+++ app/src/main/java/com/example/blackjack/CardActivity.java
@@ -1,24 +1,131 @@
 package com.example.blackjack;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+
 public class CardActivity extends AppCompatActivity{
-    int temp1 = 0;
+    Player User = new Player();
+    Player House = new Player();
+    RequestQueue requestQueue;
+    String deckid = "";
+    String draw1card = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.card_table);
 
+        requestQueue = Volley.newRequestQueue(this);
+        User.cardSlots = new ImageView[] {findViewById(R.id.imageView1), findViewById(R.id.imageView2) ,findViewById(R.id.imageView3), findViewById(R.id.imageView4) ,findViewById(R.id.imageView5),
+                findViewById(R.id.imageView6),findViewById(R.id.imageView7), findViewById(R.id.imageView8),findViewById(R.id.imageView9), findViewById(R.id.imageView10),
+                findViewById(R.id.imageView11) };
 
+        House.cardSlots = new ImageView[] { findViewById(R.id.imageView12) ,findViewById(R.id.imageView13), findViewById(R.id.imageView14) ,findViewById(R.id.imageView15),
+                findViewById(R.id.imageView16),findViewById(R.id.imageView17), findViewById(R.id.imageView18),findViewById(R.id.imageView19), findViewById(R.id.imageView20),
+                findViewById(R.id.imageView21),findViewById(R.id.imageView22) };
+
+        User.invisibility(); House.invisibility(); //ALL card slots are invisible
+
+
+        String deckURL = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+
+        //Gets a new deck and draws 2 cards for both players
+        getNewDeck(deckURL, new VolleyResponseListener() {
+            @Override
+            public void onError(String error) {
+                System.out.println(error);
+            }
+            @Override
+            public void onResponse(String response) {
+                deckid = Parse(response, "deck_id");
+                String draw2cards = "https://deckofcardsapi.com/api/deck/" + deckid + "/draw/?count=2";
+                draw1card = "https://deckofcardsapi.com/api/deck/" + deckid + "/draw/?count=1";
+                drawCard(draw2cards, User, new VolleyResponseListener() {
+                    @Override
+                    public void onError(String error) { }
+                    @Override
+                    public void onResponse(String response) {
+                        User.setValues(User.hand);
+                        setCards(User);
+                        TextView tempText = findViewById(R.id.textView3);
+                            tempText.setText("");
+                    }
+                });
+                drawCard(draw2cards, House, new VolleyResponseListener() {
+                            @Override
+                            public void onError(String error) { }
+                            @Override                       //Draw 2 cards for both Players
+                            public void onResponse(String response) {
+                                House.setValues(House.hand);
+                                setCards(House);
+                                House.cardSlots[1].setImageResource(R.drawable.gray_back);
+                            }
+                        });
+                    }
+        });
+
+                //Stay and Draw Buttons
         Button StayButton = findViewById(R.id.Stay);
         StayButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                temp1 += 1;
+                User.setValues(User.hand);
+                TextView tempText = findViewById(R.id.textView3);
+                String holder = "You Won!"; String Holder1 = "You Lost.";
+                setCards(House);
+                stayChecker();
+                /*while(House.value2 < 17 && House.value1 < 17) {
+                    drawCard(draw1card, House, new VolleyResponseListener() {
+                        @Override
+                        public void onError(String error) { System.out.println(error);
+                        }
+                        @Override
+                        public void onResponse(String response) {
+                            setCards(House);
+                            if (House.Busted()) {
+                                tempText.setText(holder);
+                                User.reset(); House.reset();
+                            }
+                        }
+                    });
+                }*/
+                setCards(House);
+                if (House.Busted()) {
+                    tempText.setText(holder);
+                    User.reset(); House.reset();
+                }
+                if (User.isWinner(House)) {
+                    tempText.setText(holder);
+                } else {
+                    tempText.setText(holder);
+                }
+                CardActivity.this.recreate();
+                User.reset(); House.reset();
             }
         });
 
@@ -26,10 +133,158 @@ public class CardActivity extends AppCompatActivity{
         DrawButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                TextView tempText = findViewById(R.id.temp);
-               tempText.setText(String.valueOf(temp1));
-
+                TextView tempText = findViewById(R.id.textView3);
+                drawCard(draw1card, User, new VolleyResponseListener() {
+                    @Override
+                    public void onError(String error) {
+                        String fail = "failed to draw";
+                        tempText.setText(fail);
+                    }
+                    @Override
+                    public void onResponse(String response) {
+                        if (!(User.setValues(User.hand))) {
+                            String holder = "You Busted.";
+                            tempText.setText(holder);
+                            setCards(User);
+                            User.reset(); House.reset();
+                            return;
+                        }
+                        setCards(User);
+                    }
+                });
             }
         });
     }
+
+
+    public interface VolleyResponseListener {
+        void onError(String error);
+        void onResponse(String response);
+    }
+
+
+    // getNewDeck gets a new deckid
+    // calls parse function
+    //
+    public void getNewDeck(String url, final VolleyResponseListener volleyResponseListener) {
+        StringRequest jsonStringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    String hold;
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                             hold = response;
+                        } catch (Exception e) {
+                            System.out.println(e);
+                        }
+                        volleyResponseListener.onResponse(hold);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        volleyResponseListener.onError("failure");
+                    }
+                });
+        requestQueue.add(jsonStringRequest);
+    }
+    //
+    //gets a card from deck and puts it into players' hands.
+    //
+    public void drawCard(String url, Player player, final VolleyResponseListener volleyResponseListener) {
+        StringRequest jsonStringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            parse2(response, player);
+
+                        } catch (Exception e) {
+                            System.out.println(e);
+                        }
+                        volleyResponseListener.onResponse(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        volleyResponseListener.onResponse("failure2");
+                    }
+                });
+        requestQueue.add(jsonStringRequest);
+    }
+    // Parse
+    // Transform response into JSONObject and then parses.
+    // response and key are used to parse.
+    private String Parse(String response, String key) {
+        String returnString;
+        try {
+            JSONObject JSONresponse = new JSONObject(response);
+                 returnString = JSONresponse.getString(key);
+               // return reference;
+        }catch (Exception e) {
+            System.out.println(e);
+            returnString = "Failure2";
+        }
+        return returnString;
+    }
+    //
+    // Transform response into JsonArray, then parses.
+    //
+    private boolean parse2(String response, Player player) {
+        try {
+            JSONObject JSONresponse = new JSONObject(response);
+            JSONArray Jsonarray = JSONresponse.getJSONArray("cards");
+            for (int i = 0; i < Jsonarray.length(); i++) {
+                JSONObject temp = Jsonarray.getJSONObject(i);
+                player.hand.add(temp.getString("value"));
+                player.code.add(temp.getString("code"));      //adds directly into both arrays
+            }
+        }catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+        return true;
+    }
+    //displays card images after on screen
+    public void setCards(Player player) {
+
+        for (int i = 0; i < player.code.size();i++) {
+
+            Resources res = this.getResources();
+            String temp = "" +player.code.get(i).charAt(1) + player.code.get(i).charAt(0); //+ ".png";
+
+            temp = temp.toLowerCase();
+            System.out.println(temp);
+            int resID = res.getIdentifier(temp , "drawable", CardActivity.this.getPackageName());
+            player.cardSlots[i].setImageResource(resID);
+            player.cardSlots[i].setVisibility(View.VISIBLE);
+        }
+    }
+    // Makes the value1 of the House greater than 17. Does not check Busted or not.
+    public void stayChecker() {
+        if (House.value2 < 17 && House.value1 < 17) {
+            drawCard(draw1card, House, new VolleyResponseListener() {
+                @Override
+                public void onError(String error) { System.out.println(error);
+                }
+                @Override
+                public void onResponse(String response) {
+                    setCards(House);
+                    House.setValues(House.hand);
+                    if (House.value2 < 17 && House.value1 < 17) {
+                         stayChecker();
+                    }
+                    return;
+                }
+            });
+        }
+        return;
+        }
 }
+
+
+
+
+
--- app/src/main/java/com/example/blackjack/Player.java
+++ app/src/main/java/com/example/blackjack/Player.java
@@ -1,19 +1,110 @@
 package com.example.blackjack;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.util.*;
 
 // User and "House",
 public class Player {
-    String name;
+    ArrayList<String> code;
     ArrayList<String> hand;
-    int value1;
-    int value2;
-    public Player(String name) {
-        this.name = name;
+    ImageView[] cardSlots;
+    //ArrayList<ImageView> cardSlots;
+    int value1; // holds default hand, ACE is equal to 11. NOTE: only one Ace can be valued at 11 in a players hand.
+    int value2; // holds value if ALL Aces must equal 1
+
+    public Player() {
+        hand = new ArrayList<String>();
+        code = new ArrayList<String>();
+      //  cardSlots = new ImageView[11];
+       // cardSlots = new ArrayList<ImageView>();
     }
 
     void reset() {
-        value1 = 0; value2 = 0; hand.clear();
+        value1 = 0; value2 = 0; hand.clear(); code.clear(); this.invisibility();
     }
+    //
+    // Goes through Arraylist and adds up all cards values. Returns false when both numbers go over 21. Player loses.
+    //Called when after Player draws a card.
+    public boolean setValues(ArrayList<String> hand) {
+        value1 = 0; value2 = 0;
+        boolean hasAce = false;
+        for(int i = 0; i < hand.size(); i++) {
+           if (isNumeric(hand.get(i))) {
+               value1 += Integer.parseInt(hand.get(i));   //Handles numeric cards except Ace
+               value2 += Integer.parseInt(hand.get(i));
+           } else if (hand.get(i).charAt(0) == 'A') {
+                if (hasAce == false) {
+                    value1 += 11; value2+=1; //Handles Aces
+                    hasAce = true;
+                    if (this.Busted()) {
+                        return false;
+                    }
+                    continue;
+                }
+                value1++; value2++;
+           } else {
+               value1+=10; value2+=10; //Handles face cards
+           }
+           if (this.Busted()) {
+               return false;
+           }
+        }
+        return true;
+    }
+    //
+    //checks which numbers are higher. Does not account for over 21. Check if busted before this.
+    //
+    public boolean isWinner(Player other) {
+        int finalValue1; int finalValue2;
+        if (this.value1 >= this.value2) {
+            finalValue1 = this.value1;
+        } else {
+            finalValue1 = this.value2;
+        }
+
+        if(other.value1  >= other.value2) {
+            finalValue2 = other.value1;
+        } else {
+            finalValue2 = other.value2;
+        }
+
+        if(finalValue1  >= finalValue2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //
+    //Checks if card value stored in ArrayList hand is numeric
+    //
+    private boolean isNumeric(String card) {
+        try {
+           int a = Integer.parseInt(card);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+    //
+    // checks if Player busted after drawing a card
+    //
+    public boolean Busted() {
+        if (value1 > 21 && value2 > 21) {
+            return true;
+        }
+        return false;
+    }
+    //sets all all images in cardSlots to ivinsible called at start of every new game.
+    public void invisibility() {
+        for (ImageView i : cardSlots) {
+            i.setVisibility(View.INVISIBLE);
+        }
+    }
+
+
 }
